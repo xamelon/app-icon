ab0b1d1c718c0f6b2db79acc33645502d3e58cb6
Stas Buldakov <xamelon96@gmail.com>
2021-01-08 02:07:47 +0400

    fix: progress

--- resources/public/js/cljs-runtime/app.index.core.js
+++ resources/public/js/cljs-runtime/app.index.core.js
@@ -1,17 +1,65 @@
 goog.provide('app.index.core');
+if((typeof app !== 'undefined') && (typeof app.index !== 'undefined') && (typeof app.index.core !== 'undefined') && (typeof app.index.core.image !== 'undefined')){
+} else {
+app.index.core.image = reagent.core.atom.cljs$core$IFn$_invoke$arity$1(null);
+}
 app.index.core.on_click_file = (function app$index$core$on_click_file(){
 return document.getElementById("icon-file").click();
 });
+app.index.core.on_read_file = (function app$index$core$on_read_file(e){
+console.log(e);
+
+var result = e.target.result;
+return cljs.core.reset_BANG_(app.index.core.image,result);
+});
 app.index.core.on_load_file = (function app$index$core$on_load_file(event){
-var file = document.getElementById("icon-file").files();
-return cljs.core.println.cljs$core$IFn$_invoke$arity$variadic(cljs.core.prim_seq.cljs$core$IFn$_invoke$arity$2([file], 0));
+var file = document.getElementById("icon-file").files;
+var reader = (new FileReader());
+(reader.onload = (function (p1__58156_SHARP_){
+return app.index.core.on_read_file(p1__58156_SHARP_);
+}));
+
+return reader.readAsDataURL(cljs.core.first(file));
+});
+app.index.core.draw_to_canvas = (function app$index$core$draw_to_canvas(e){
+console.log(e);
+
+var result = e.target.result;
+var canvas = document.createElement("canvas");
+var context = canvas.getContext("2d");
+var image = (new Image("1024px","1024px"));
+(image.src = result);
+
+context.drawImage(image,(0),(0),"1024px","1024px");
+
+context.fillRect((20),(20),(20),(20));
+
+var img = canvas.toDataURL();
+var link = document.createElement("a");
+(link.download = "filename.png");
+
+(link.href = img);
+
+return link.click();
+});
+app.index.core.generate_icons = (function app$index$core$generate_icons(){
+var svg = document.getElementById("generated-app-icon");
+var blob = (new Blob([svg.outerHTML],({"type": "image/svg+xml;charset=utf-8"})));
+var reader = (new FileReader());
+(reader.onloadend = (function (p1__58157_SHARP_){
+return app.index.core.draw_to_canvas(p1__58157_SHARP_);
+}));
+
+return reader.readAsDataURL(blob);
 });
 app.index.core.core = (function app$index$core$core(){
-return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"<>","<>",1280186386),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h1","h1",-1896887462),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),new cljs.core.Keyword(null,"app_index_core-19-17","app_index_core-19-17",35043461)], null),"This is app icon generator"], null),new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"on-click","on-click",1632826543),(function (){
+return new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"<>","<>",1280186386),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h1","h1",-1896887462),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),new cljs.core.Keyword(null,"app_index_core-53-17","app_index_core-53-17",338216078)], null),"This is app icon generator"], null),new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"on-click","on-click",1632826543),(function (){
 return app.index.core.on_click_file();
-}),new cljs.core.Keyword(null,"class","class",-2030961996),new cljs.core.Keyword(null,"app_index_core-21-18","app_index_core-21-18",1307991588)], null),"Drag and drop here",new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),new cljs.core.Keyword(null,"app_index_core-33-19","app_index_core-33-19",916064381)], null),"or clic to choose file"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"input","input",556931961),new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"id","id",-1388402092),"icon-file",new cljs.core.Keyword(null,"on-change","on-change",-732046149),(function (p1__73495_SHARP_){
-return app.index.core.on_load_file(p1__73495_SHARP_);
-}),new cljs.core.Keyword(null,"type","type",1174270348),"file",new cljs.core.Keyword(null,"class","class",-2030961996),new cljs.core.Keyword(null,"app_index_core-38-21","app_index_core-38-21",1772807171)], null)], null)], null)], null);
+}),new cljs.core.Keyword(null,"class","class",-2030961996),new cljs.core.Keyword(null,"app_index_core-56-18","app_index_core-56-18",998487009)], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"input","input",556931961),new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"id","id",-1388402092),"icon-file",new cljs.core.Keyword(null,"on-change","on-change",-732046149),(function (p1__58158_SHARP_){
+return app.index.core.on_load_file(p1__58158_SHARP_);
+}),new cljs.core.Keyword(null,"type","type",1174270348),"file",new cljs.core.Keyword(null,"class","class",-2030961996),new cljs.core.Keyword(null,"app_index_core-69-21","app_index_core-69-21",-1314403770)], null)], null),(cljs.core.truth_(cljs.core.deref(app.index.core.image))?new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"svg","svg",856789142),new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"width","width",-384071477),"100%",new cljs.core.Keyword(null,"height","height",1025178622),"100%",new cljs.core.Keyword(null,"id","id",-1388402092),"generated-app-icon"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"image","image",-58725096),new cljs.core.PersistentArrayMap(null, 5, [new cljs.core.Keyword(null,"href","href",-793805698),cljs.core.deref(app.index.core.image),new cljs.core.Keyword(null,"x","x",2099068185),"0",new cljs.core.Keyword(null,"y","y",-1757859776),"0",new cljs.core.Keyword(null,"width","width",-384071477),"100%",new cljs.core.Keyword(null,"height","height",1025178622),"100%"], null)], null),new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"g","g",1738089905),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"transform","transform",1381301764),"rotate(-45 50 50)"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"rect","rect",-108902628),new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"fill","fill",883462889),"grey",new cljs.core.Keyword(null,"width","width",-384071477),"150",new cljs.core.Keyword(null,"height","height",1025178622),"40",new cljs.core.Keyword(null,"x","x",2099068185),"-20"], null)], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"text","text",-1790561697),new cljs.core.PersistentArrayMap(null, 5, [new cljs.core.Keyword(null,"x","x",2099068185),"53",new cljs.core.Keyword(null,"y","y",-1757859776),"23",new cljs.core.Keyword(null,"text-anchor","text-anchor",585613696),"middle",new cljs.core.Keyword(null,"alignment-baseline","alignment-baseline",-311060879),"middle",new cljs.core.Keyword(null,"fill","fill",883462889),"white"], null),"DEMO"], null)], null)], null):new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),"Drag and drop here",new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),new cljs.core.Keyword(null,"app_index_core-94-22","app_index_core-94-22",-1065543299)], null),"or clic to choose file"], null)], null))], null),(cljs.core.truth_(cljs.core.deref(app.index.core.image))?new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"a","a",-2123407586),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"on-click","on-click",1632826543),(function (){
+return app.index.core.generate_icons();
+})], null),"Download"], null):null)], null);
 });
 
 //# sourceMappingURL=app.index.core.js.map
--- src-ui/app/index/core.cljc
+++ src-ui/app/index/core.cljc
@@ -1,38 +1,103 @@
 (ns app.index.core
-  (:require [stylo.core :refer [c]])
+  (:require [stylo.core :refer [c]]
+            [reagent.core :as r])
   )
 
+
+(defonce image (r/atom nil))
 
 (defn on-click-file []
   #?(:cljs
      (.click (.getElementById js/document "icon-file"))))
 
+(defn on-read-file [^js/ProgressEvent e]
+  (.log js/console e)
+  (let [result (.-result (.-target e))]
+    (reset! image result)
+    ))
+
 
 (defn on-load-file [event]
-  (let [file #?(:cljs (.files (.getElementById js/document "icon-file")))]
-    (println file)
+  (let [file #?(:cljs (.-files (.getElementById js/document "icon-file")))
+        reader (js/FileReader.)]
+    (set! (.-onload reader) #(on-read-file %))
+    (.readAsDataURL reader (first file))))
+
+(defn draw-to-canvas [e]
+  (.log js/console e)
+  (let [result (.-result (.-target e))
+        canvas (.createElement js/document "canvas")
+        context (.getContext canvas "2d")
+        image (js/Image. "1024px" "1024px")]
+    (set! (.-src image) result)
+    (.drawImage context image 0 0 "1024px" "1024px")
+    (.fillRect context 20 20 20 20)
+    (let [img (.toDataURL canvas)
+          link (.createElement js/document "a")]
+      (set! (.-download link) "filename.png")
+      (set! (.-href link) img)
+      (.click link)
+      )
     )
   )
+
+(defn generate-icons []
+  (let [svg (.getElementById js/document "generated-app-icon")
+        blob (js/Blob. #js[(.-outerHTML svg)] #js{:type "image/svg+xml;charset=utf-8"})
+        reader (js/FileReader.)]
+    (set! (.-onloadend reader) #(draw-to-canvas %))
+    (.readAsDataURL reader blob)))
 
 (defn core []
   [:<>
    [:h1 {:class (c :italic)}  "This is app icon generator"]
+   
    [:div {:on-click #(on-click-file)
-          :class (c :flex
-                    :flex-col
-                    [:w "30%"]
-                    [:h "30%"]
+          :class (c [:w "40vh"]
+                    [:h "40vh"]
                     [:rounded 16]
+                    :overflow-hidden
                     :border-dashed
                     [:border 3 :gray-500]
                     :cursor-pointer
                     :items-center
                     :justify-center
                     :text-lg)}
-    "Drag and drop here"
-    [:div {:class (c :text-sm [:text :gray-900])}
-     "or clic to choose file"]
     [:input {:id "icon-file"
              :on-change #(on-load-file %)
              :type "file"
-             :class (c :hidden)}]]])
+             :class (c :hidden)}]
+    (if @image
+      [:svg
+       {:width "100%" :height "100%"
+        :id "generated-app-icon"}
+       [:image {:href @image
+                :x "0"
+                :y "0"
+                :width "100%"
+                :height "100%"}]
+       [:g {:transform "rotate(-45 50 50)"}
+        [:rect {:fill "grey"
+                :width "150"
+                :height "40"
+                :x "-20"}]
+        [:text {:x "53"
+                :y "23"
+                :text-anchor "middle"
+                :alignment-baseline "middle"
+                :fill "white"}
+         "DEMO"
+         ]]
+       ]
+      [:div 
+       "Drag and drop here"
+       [:div {:class (c :text-sm [:text :gray-900])}
+        "or clic to choose file"]
+       ])    
+    ]
+   
+   (when @image
+     [:a {:on-click #(generate-icons)} "Download"])
+   
+   ])
+
